de483acae23d6009e817d2fa60a1efa541f72164
Ahsen Ceyda Altaş <64913741+AhsenAltas@users.noreply.github.com>
2020-12-30 17:55:12 +0300

    Proje

--- Sport Proje/assets/js/comments.js
+++ Sport Proje/assets/js/comments.js
@@ -1,15 +1,17 @@
-function render(data) {
+
+function render(data1) {
     
-            var html = "<div class='commentBox'><div class='rightPanel'><span>" + data.name + "</span><p>" + data.body + "</p></div><div class='clear'></div></div>";
+            var html = "<div class='commentBox'><div class='rightPanel'><span>" + data1.UserName + "</span><p>" + data1.body + "</p></div><div class='clear'></div></div>";
     
-            $('#container').append(html);
-    
+            $('#container1').append(html);
+			
+			
         }
     
         $(document).ready(function() {
     
             var coment = [{
-                "name": "",
+                "UserName": "",
                 "body": ""
             }];
     
@@ -18,15 +20,49 @@ function render(data) {
     
             }
     
-            $('#addComent').click(function() {
+            $('#addComent1').click(function() {
                 var addObj = {
-                    "name": $('#name').val(),
-                    "body": $('#bodyText').val()
+                    "name": $('#UserName').val(),
+                    "body": $('#bodyText1').val()
                 };
     
                 coment.push(addObj);
                 render(addObj);
-                $('#name').val('');
-                $('#bodyText').val('');
+                $('#UserName').val('');
+                $('#bodyText1').val('');
             });
         });
+		
+
+function render(data2) {
+    
+            var html2 = "<div class='commentBox'><div class='rightPanel'><span>" + data2.UserName + "</span><p>" + data2.body + "</p></div><div class='clear'></div></div>";
+    
+            $('#container2').append(html2);
+			
+        }
+		
+        $(document).ready(function() {
+    
+            var coment2 = [{
+                "UserName": "",
+                "body": ""
+            }];
+    
+            for (var k = 0; k < coment2.length; k++) {
+                render(coment2[k]);
+    
+            }
+    
+            $('#addComent2').click(function() {
+                var addObj2 = {
+                    "name": $('#UserName').val(),
+                    "body": $('#bodyText2').val()
+                };
+    
+                coment2.push(addObj2);
+                render(addObj2);
+                $('#UserName').val('');
+                $('#bodyText2').val('');
+            });
+        });		
