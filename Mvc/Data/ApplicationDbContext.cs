105e405224b851c6d59dc739dd28422f0ea21199
Ahsen Ceyda Altaş <64913741+AhsenAltas@users.noreply.github.com>
2021-01-03 22:58:37 +0300

    Proje

---
+++ Mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SportProject.Models;
+
+namespace SportProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Member>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Sports> Sports { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+        public DbSet<Courses> Courses { get; set; }
+    }
+}
---
+++ Mvc/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportProject.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string CommentTxt { get; set; }
+        public DateTime SendDate { get; set; }
+        public bool ComApproval { get; set; }
+
+        public int CourseId { get; set; }
+        public Courses Courses { get; set; }
+
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+    }
+}
